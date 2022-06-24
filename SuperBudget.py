f95912c868d3e07f8d0bee592b037747d422977e
Tayler Uva <8679670+TaylerUva@users.noreply.github.com>
2022-06-24 16:33:44 -0700

    changed naming conventions to be more clear

--- SuperBudget.py
+++ SuperBudget.py
@@ -11,25 +11,25 @@ from funcs.data_display import display_totals, display_df_split_by_time
 
 # %% [markdown]
 # ## Validation Outputs
-spend_df = get_pdf_totals(pdf_file_paths='file_paths.csv', vendor_categories="vendor_category.csv")
-spend_df.to_csv('Total Costs.csv', index=False)
+df_spend = get_pdf_totals(pdf_file_paths='file_paths.csv', vendor_categories="vendor_category.csv")
+df_spend.to_csv('Total Costs.csv', index=False)
 
 # %% [markdown]
 # ### Receipts
-display(spend_df.sort_values('total', ascending=False))
+display(df_spend.sort_values('total', ascending=False))
 
 # %% [markdown]
 # ## Balance
-display_totals(spend_df, income_df=pd.read_csv("income.csv"), str_carry_over_tag='CarryOver')
+display_totals(df_spend=df_spend, df_income=pd.read_csv("income.csv"), str_carry_over_tag='CarryOver')
 
 # %% [markdown]
 # ## Vendor Breakdown
-display(spend_df[['vendor', 'total']].groupby('vendor').sum().sort_values('total', ascending=False))
+display(df_spend[['vendor', 'total']].groupby('vendor').sum().sort_values('total', ascending=False))
 
 # %% [markdown]
 # ## Vendor Category Breakdown
-display(spend_df.groupby('category').sum().sort_values('total', ascending=False))
+display(df_spend.groupby('category').sum().sort_values('total', ascending=False))
 
 # %% [markdown]
 # ## Reimbursement Calculations
-display_df_split_by_time(spend_df, date_cutoff='3/1/22')
+display_df_split_by_time(df_spend=df_spend, date_cutoff='3/1/22')
--- funcs/data_display.py
+++ funcs/data_display.py
@@ -2,23 +2,23 @@ from IPython.display import display
 from pandas import DataFrame
 
 
-def display_df_split_by_time(spend_df: DataFrame, date_cutoff: str) -> None:
-    pre_cuttoff_df = spend_df.loc[spend_df['date'] < date_cutoff]
+def display_df_split_by_time(df_spend: DataFrame, date_cutoff: str) -> None:
+    df_pre_cuttoff = df_spend.loc[df_spend['date'] < date_cutoff]
     print(f'Pre {date_cutoff}')
-    print('Total:', pre_cuttoff_df['total'].sum())
-    display(pre_cuttoff_df)
+    print('Total:', df_pre_cuttoff['total'].sum())
+    display(df_pre_cuttoff)
 
-    post_cuttoff_df = spend_df.loc[spend_df['date'] >= date_cutoff]
+    post_cuttoff_df = df_spend.loc[df_spend['date'] >= date_cutoff]
     print(f'Post {date_cutoff}')
     print('Total:', post_cuttoff_df['total'].sum())
     display(post_cuttoff_df)
 
 
-def display_totals(spend_df: DataFrame, income_df: DataFrame, str_carry_over_tag: str = 'CarryOver') -> None:
-    carry_over_mask = (income_df['sponsor'] == str_carry_over_tag)
-    print('+ Total Income:\t\t %.2f' % income_df['amount'].sum())
-    print('\t| New Income\t %.2f' % income_df.loc[~carry_over_mask]['amount'].sum())
-    print('\t| Carry Over\t %.2f' % income_df.loc[carry_over_mask]['amount'].sum())
-    print('- Total Spent:\t\t %.2f' % spend_df['total'].sum())
+def display_totals(df_spend: DataFrame, df_income: DataFrame, str_carry_over_tag: str = 'CarryOver') -> None:
+    mask_carry_over = (df_income['sponsor'] == str_carry_over_tag)
+    print('+ Total Income:\t\t %.2f' % df_income['amount'].sum())
+    print('\t| New Income\t %.2f' % df_income.loc[~mask_carry_over]['amount'].sum())
+    print('\t| Carry Over\t %.2f' % df_income.loc[mask_carry_over]['amount'].sum())
+    print('- Total Spent:\t\t %.2f' % df_spend['total'].sum())
     print('===================')
-    print('Remaining Balance:\t %.2f' % (income_df['amount'].sum()-spend_df['total'].sum()))
+    print('Remaining Balance:\t %.2f' % (df_income['amount'].sum()-df_spend['total'].sum()))
